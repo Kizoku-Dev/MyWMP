bbb66fd5ee6ac3dc82d5fa46103212f364ff5394
Noël DO VAN <noel.dovan@epitech.eu>
2014-12-17 22:31:52 +0100

    simple version work ok

---
+++ Simple/WMP/WMP/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace WMP
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ViewModel _viewModel;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Init Model
+
+            // Init View
+            media1.LoadedBehavior = MediaState.Manual;
+            media1.Volume = (double) sliderVolume.Value;
+            
+            // Init ViewModel
+            this._viewModel = new ViewModel();
+        }
+
+        private void btnPlay_Click(object sender, RoutedEventArgs e)
+        {
+            if (media1.Source != null)
+                this._viewModel.btnPlay_Click(sender, e, media1, sliderMedia);
+        }
+
+        private void btnStop_Click(object sender, RoutedEventArgs e)
+        {
+            if (media1.Source != null)
+                this._viewModel.btnStop_Click(sender, e, media1);
+        }
+
+        private void btnOpen_Click(object sender, RoutedEventArgs e)
+        {
+            this._viewModel.btnOpen_Click(sender, e, media1, sliderMedia);
+        }
+
+        private void slideMedia_MouseDown(object sender, RoutedEventArgs e)
+        {
+            this._viewModel.slideMedia_MouseDown(sender, e, media1);
+        }
+
+        private void slideMedia_MouseUp(object sender, RoutedEventArgs e)
+        {
+            this._viewModel.slideMedia_MouseUp(sender, e, media1, sliderMedia);
+        }
+
+        private void slideVolume_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            media1.Volume = (double)sliderVolume.Value;
+        }
+
+        private void media1_MediaOpened(object sender, RoutedEventArgs e)
+        {
+            this._viewModel.media1_MediaOpened(sender, e, media1, sliderMedia);
+        }
+    }
+}
---
+++ Simple/WMP/WMP/ViewModel.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Forms;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+
+namespace WMP
+{
+    class ViewModel
+    {
+        private bool _slideChanged;
+
+        public void btnPlay_Click(object sender, RoutedEventArgs e,
+            MediaElement media1, Slider sliderMedia)
+        {
+            media1.Play();
+            MediaClock clock = media1.Clock;
+            if (clock != null)
+            {
+                if (clock.IsPaused)
+                    clock.Controller.Resume();
+                else
+                    clock.Controller.Pause();
+            }
+            else
+            {
+                MediaTimeline timeline = new MediaTimeline(media1.Source);
+                clock = timeline.CreateClock();
+                clock.CurrentTimeInvalidated += new EventHandler((sender1, e1) => Clock_CurrentTimeInvalidated(sender, e, media1, sliderMedia));
+                media1.Clock = clock;
+            }
+        }
+
+        public void Clock_CurrentTimeInvalidated(object sender, EventArgs e,
+            MediaElement media1, Slider sliderMedia)
+        {
+            if (media1.Clock == null || this._slideChanged)
+                return;
+            if (media1.Clock.CurrentTime != null)
+                sliderMedia.Value = media1.Clock.CurrentTime.Value.TotalMilliseconds;
+        }
+
+        public void btnStop_Click(object sender, RoutedEventArgs e,
+            MediaElement media1)
+        {
+            media1.Stop();
+            media1.Clock = null;
+        }
+
+        public void btnOpen_Click(object sender, RoutedEventArgs e,
+            MediaElement media1, Slider sliderMedia)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.AddExtension = true;
+            ofd.DefaultExt = "*.*";
+            ofd.Filter = "Media Files (*.*)|*.*";
+            ofd.ShowDialog();
+
+            try { media1.Source = new Uri(ofd.FileName); }
+            catch { new NullReferenceException("Error"); }
+
+            this.btnStop_Click(sender, e, media1);
+            this.btnPlay_Click(sender, e, media1, sliderMedia);
+        }
+
+        public void slideMedia_MouseDown(object sender, RoutedEventArgs e,
+            MediaElement media1)
+        {
+            this._slideChanged = true;
+
+        }
+
+        public void slideMedia_MouseUp(object sender, RoutedEventArgs e,
+            MediaElement media1, Slider sliderMedia)
+        {
+            MediaClock clock = media1.Clock;
+            if (clock != null)
+            {
+                TimeSpan mOffset = TimeSpan.FromMilliseconds(sliderMedia.Value);
+                if (clock.Controller != null) clock.Controller.Seek(mOffset, TimeSeekOrigin.BeginTime);
+            }
+            this._slideChanged = false;
+        }
+
+        public void media1_MediaOpened(object sender, RoutedEventArgs e,
+            MediaElement media1, Slider sliderMedia)
+        {
+            if (media1.NaturalDuration.HasTimeSpan)
+                sliderMedia.Maximum = media1.NaturalDuration.TimeSpan.TotalMilliseconds;
+        }
+
+    }
+}
